10d285ce76a27e604bd38b00779a6f8f2369266e
kouyoumikan <kouyoumikan136@gmail.com>
2020-09-11 17:14:10 +0900

    カテゴリー画面のレイアウト1

--- kuizu_app/lib/screens/category/category.dart
+++ kuizu_app/lib/screens/category/category.dart
@@ -28,6 +28,233 @@ class _CategoryState extends State<Category> {
           ),
         ],
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.gps_fixed, // 地図アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.wb_sunny, // 天気アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.error, // マップアイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+            ],
+          ),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: <Widget>[
+          //     Text("地図記号"),
+          //     Text("地図記号"),
+          //     Text("地図記号"),
+          //   ],
+          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.music_note, // 音楽アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.star, // 星座アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.star_half, // 家紋アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.filter_vintage, // 日本文様アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.adjust, // ヒエログリフアイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 130.0,
+                height: 100.0,
+                child: new RaisedButton(
+                  color: Colors.white, // ボタンの背景色
+                  child: Icon(
+                    Icons.star_half, // 天体アイコン
+                    color: Colors.lightBlue,
+                    size: 50.0,
+                  ),
+                  shape: CircleBorder( // 円形
+                    side: BorderSide( // 枠線
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  onPressed: () { // 指定先へ移動
+                    Navigator.pushNamed(context, '/main');
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      )
     );
   }
 }
